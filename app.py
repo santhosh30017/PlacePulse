8ca5be934e15deeb42d294fd93fb9a36097a51dc
santhosh3017 <santhoshp24cs@srishakthi.ac.in>
2026-09-24 21:07:13 +0530

    Prepare PlacePulse for Render deployment

--- app.py
+++ app.py
@@ -450,4 +450,4 @@ def status():
 if __name__ == '__main__':
     init_db()
     initialize_pipeline()
-    app.run(debug=True, port=5050, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5050)), use_reloader=False)
